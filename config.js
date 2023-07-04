0a04aafcbc30c811b6cbe043f9738b575753c525
MosiOaTunya <65680197+MosiOaTunya@users.noreply.github.com>
2023-07-04 10:30:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['0638396602', 'Illy', true],
+  ['0777720779, , 'bot', true], 
   [''] 
 ] //Number of owners
 
